d2f0359e3ccde419e6107b38e5240fbbb063631d
moonstuff <rocksandthings@themoon.com>
2011-04-05 22:00:12 +1000

    LICENSE, wotevs, and a derpaderp fix

--- derpify/derpify.rb
+++ derpify/derpify.rb
@@ -11,7 +11,11 @@ salt = rand(2**64).to_s
 
 ARGF.each do |line|
   row = line.split("\t")
-  row[4] = row[4].chomp.split.map { |s| Digest::SHA2.hexdigest(s + salt) }.join(" ")
+  if row[3] == "appearance" or row[3] == "click" then
+    row[4] = Digest::SHA2.hexdigest(row[4] + salt) 
+  elsif row[3] == "search"
+    row[4] = row[4].chomp.split.map { |s| Digest::SHA2.hexdigest(s + salt) }.join(" ")
+  end
   STDOUT << row.join("\t").chomp << "\n"
 end
 
