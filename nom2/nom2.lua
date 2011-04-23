ac594daef9bd88265654556a778352976e6ce9a4
moonstuff <rocksandthings@themoon.com>
2011-04-23 10:15:14 +1000

    nom2 with kyotocabinet, almost directly comparable to the C version

--- nom2/nom2.lua
+++ nom2/nom2.lua
@@ -1,33 +1,30 @@
 #!/usr/bin/env luajit
-  
--- No string splitter? *sigh*
--- This is from the lua users wiki where *everyone* goes to get this function.
-function string:split(sep)
-  local sep, fields = sep or ":", {}
-  local pattern = string.format("([^%s]+)", sep)
-  self:gsub(pattern, function(c) fields[#fields+1] = c end)
-  return fields
+
+require "stringy"
+kc = require "kyotocabinet"
+
+if #arg ~= 3 then
+  print("usage: nom2 <appearances database> <urls database> <url reports database>")
+  return 1
 end
 
-function store_url(url)
-  if urls[url] == nil then
-    urls[url] = 1
-    stats.urls = stats.urls + 1
-  else
-    urls[url] = urls[url] + 1
-  end
+appearances_filename = arg[1]
+appearances = kc.DB:new()
+if not appearances:open(appearances_filename, kc.DB.OWRITER + kc.DB.OCREATE) then
+   print("appearances database open error: " .. tostring(appearances:error()))
 end
 
-function store_appearance(url, date)
-  appearances_for_url = appearances[url] or {}
-  appearances_for_url[date] = (appearances_for_url[date] or 0) + 1
-  appearances[url] = appearances_for_url
+urls_filename = arg[2]
+urls = kc.DB:new()
+if not urls:open(urls_filename, kc.DB.OWRITER + kc.DB.OCREATE) then
+   print("urls database open error: " .. tostring(urls:error()))
 end
 
--- No need for KyotoCabinet for the moment, Lua is hashes all the way down :)
-urls = {}
-appearances = {}
-reports_filename = arg[1]
+url_reports_filename = arg[3]
+url_reports = kc.DB:new()
+if not url_reports:open(url_reports_filename, kc.DB.OWRITER + kc.DB.OCREATE) then
+   print("url_reports database open error: " .. tostring(url_reports:error()))
+end
 
 stats = {
   lines = 0,
@@ -38,36 +35,38 @@ stats = {
 }
 
 for line in io.lines() do
-  local event = string.split(line, "\t")
+  local event = stringy.split(line, "\t")
   local event_type = event[4]
 
   if event_type == "appearance" then
     stats.appearances = stats.appearances + 1
     url = event[5]
-    date = string.split(event[3], " ")[1]
-    store_appearance(url, date)
-    store_url(url)
+    date = stringy.split(event[3], " ")[1]
+    appearances:increment(string.format("%s-%s",url,date), 1)
+    urls:increment(url, 1)
   elseif event_type == "click" then
     stats.clicks = stats.clicks + 1
-    store_url(event[5])
+    urls:increment(event[5], 1)
   elseif event_type == "search" then
     stats.searches = stats.searches + 1  
   end
   stats.lines = stats.lines + 1
 end
+stats.urls = urls:count()
 
 print(string.format("EOF reached: %d records", stats.lines))
 print(string.format("%d searches, %d appearances, %d clicks, %d urls", stats.searches, stats.appearances, stats.clicks, stats.urls))
 print(string.format("generating url reports for %d urls", stats.urls))
 
 -- Now print the reports!
-reports = io.open(reports_filename, "w+")
-for url in pairs(urls) do
-  if not (appearances[url] == nil) then
-    reports:write(url, "\n")
-    for date, appearance_count in pairs(appearances[url]) do
-      reports:write(date, ": ", appearance_count, "\n")
-    end
-  end
+for url, url_count in urls:pairs() do
+   local appearance_aggregates = appearances:match_prefix(url)
+   if #appearance_aggregates > 0 then
+     local url_report = url .. "\n"
+     for index, key in pairs(appearance_aggregates) do
+       url_report = url_report .. string.format("%s: %s\n", key:sub(#url + 2), appearances:increment(key, 0))
+     end
+     url_reports[url] = url_report
+   end
 end
 
