d52eea1e0788909887252f4a9e2063ac12e12b2a
moonstuff <rocksandthings@themoon.com>
2011-04-25 19:50:54 +1000

    splitty takes the new record format

--- splitty/splitty.lua
+++ splitty/splitty.lua
@@ -1,12 +1,15 @@
 #!/usr/bin/env luajit
 
+-- Split a giant input file into lots of day files.
+-- It'll even do some gzip for you :)
+
 require "stringy"
 gzip = require "gzip"
 files = {}
 
 for line in io.lines() do
   local event = stringy.split(line, "\t")
-  local date = stringy.split(event[3], " ")[1]
+  local date = stringy.split(event[4], " ")[1]
 
   if not files[date] then
     files[date] = gzip.open("events-" .. date .. ".gz", "a+")
